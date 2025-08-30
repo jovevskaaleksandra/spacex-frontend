182daba0a5526291f61023e087064e6217d86242
Aleksandra Jovevska <74111425+jovevskaaleksandra@users.noreply.github.com>
2025-08-30 23:42:44 +0200

    Add redirect to sign in page when sign up was successful

--- src/app/components/sign-up/sign-up.ts
+++ src/app/components/sign-up/sign-up.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,7 +14,7 @@ export class SignUpComponent {
   form: FormGroup;
   message = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.form = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       lastName: ['', [Validators.required, Validators.minLength(2)]],
@@ -29,7 +30,12 @@ export class SignUpComponent {
     }
 
     this.authService.signUp(this.form.value).subscribe({
-      next: (res: any) => (this.message = res.message || 'User created successfully'),
+      next: (res: any) => {
+        this.message = res.message || 'User created successfully';
+        setTimeout(() => {
+          this.router.navigate(['/signin']);
+        }, 1500);
+      },
       error: (err) => {
         if (err.error?.errors) {
           const allErrors = [];
